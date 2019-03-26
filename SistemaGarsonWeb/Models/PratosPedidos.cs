90f14f4f7ec9dacf96ebed17e1b92f3d65146111
disney teixeira <dineyteixeira@gmail.com>
2019-03-26 14:19:31 -0400

    Adicionando a Model PratosPedidos

---
+++ SistemaGarsonWeb/Models/PratosPedidos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SistemaGarsonWeb.Models {
+    public class PratosPedidos {
+        [Key] [ForeignKey("Prato")]
+        public int IdPrato { get; set; }
+        public virtual Prato Prato { get; set; }
+        [ForeignKey("Pedido")]
+        public int IdPedido { get; set; }
+        public virtual Pedido Pedido { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
