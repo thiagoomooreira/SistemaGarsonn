eded4aa99262133cfee9b90007b8a5ce05ca78fd
disney teixeira <dineyteixeira@gmail.com>
2019-03-26 13:57:11 -0400

    Adicionando a Model Colaborador

---
+++ SistemaGarsonWeb/Models/Colaborador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SistemaGarsonWeb.Models {
+    public class Colaborador {
+        [Key]
+        public int IdColaborador { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Funcao { get; set; }
+        public string Telefone { get; set; }
+    }
+}
