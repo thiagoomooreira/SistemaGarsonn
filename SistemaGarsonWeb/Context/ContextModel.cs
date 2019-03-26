0323dbbbaf5c5e59fa5c77a91a917238e848ad46
thiagoomooreira <thiagomsantos45@gmail.com>
2019-03-26 08:55:05 -0400

    ContextModel criada

---
+++ SistemaGarsonWeb/Context/ContextModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SistemaGarsonWeb.Context {
+    public class ContextModel: DbContext {
+        public ContextModel() : base("SistemaDB") {}
+    }
+}
