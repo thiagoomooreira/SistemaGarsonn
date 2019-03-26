d2a74b267143f8bda047f85463828636159ec29d
disney teixeira <dineyteixeira@gmail.com>
2019-03-26 13:50:45 -0400

    Adicionando a Model Prato.

---
+++ SistemaGarsonWeb/Models/Prato.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SistemaGarsonWeb.Models {
+    public class Prato {
+        [Key]
+        public int IdPrato { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
