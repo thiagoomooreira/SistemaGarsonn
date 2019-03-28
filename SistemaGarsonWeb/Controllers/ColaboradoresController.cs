08df5a46a3cd5cb85c93e6e48a30aee0ac7b795e
Gabriel <gabrielcs482@gmail.com>
2019-03-27 21:41:20 -0300

    Listagem de colaborador - gabriel

--- SistemaGarsonWeb/Controllers/ColaboradoresController.cs
+++ SistemaGarsonWeb/Controllers/ColaboradoresController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SistemaGarsonWeb.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,13 +12,10 @@ namespace SistemaGarsonWeb.Controllers
         // GET: Colaboradores
         public ActionResult Index()
         {
-            return View();
+            return View(new ColaboradorVM());
         }
 
-        [HttpPost]
-        public ActionResult Cadastrar() {
-            return null;
-        }
+
 
     }
 }
---
+++ SistemaGarsonWeb/ViewModel/ColaboradorVM.cs
@@ -0,0 +1,27 @@
+﻿using SistemaGarsonWeb.Context;
+using SistemaGarsonWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaGarsonWeb.ViewModel {
+    public class ColaboradorVM {
+        ContextModel _db = new ContextModel();
+
+        public ColaboradorVM() {
+            Colaboradores = _db.Colaboradors.ToList();
+        }
+
+        public Colaborador Colaborador { get; set; }
+        public List<Colaborador> Colaboradores { get; set; }
+        public string Campo { get; set; }
+
+        public List<Colaborador> ListarColaboradores() {
+            if(Colaboradores == null) {
+                _db.Colaboradors.ToList();
+            }
+            return Colaboradores;
+        }
+    }
+}
