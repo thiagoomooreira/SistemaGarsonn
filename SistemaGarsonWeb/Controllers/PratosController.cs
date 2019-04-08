fed9338ed80694845114ca16a274886603e54307
thiagoomooreira <thiagomsantos45@gmail.com>
2019-04-08 10:44:36 -0400

    Cadastro de pratos concluidos

--- SistemaGarsonWeb/Controllers/PratosController.cs
+++ SistemaGarsonWeb/Controllers/PratosController.cs
@@ -12,6 +12,7 @@ namespace SistemaGarsonWeb.Controllers
 {
     public class PratosController : Controller
     {
+        ContextModel _db = new ContextModel();
         // GET: Pratos
         public ActionResult Index()
         {
@@ -19,6 +20,17 @@ namespace SistemaGarsonWeb.Controllers
         }
         public ActionResult Cadastrar() {
             return View(new PratoVM());
+        }
+        [HttpPost]
+        public ActionResult Cadastrar([Bind(Include = "Prato")]PratoVM pratoVM) {
+            try {
+                _db.Pratos.Add(pratoVM.Prato);
+                _db.SaveChanges();
+                return View("Index", new PratoVM());
+            }
+            catch {
+                return View("Index", new PratoVM());
+            }
         }
         public ActionResult Buscar(string campo) {
             PratoVM Campo = new PratoVM { Campo = campo };
