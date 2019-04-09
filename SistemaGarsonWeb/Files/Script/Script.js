110f610da87346850cc5ae36218b68bce7e6a694
thiagoomooreira <thiagomsantos45@gmail.com>
2019-04-09 10:14:46 -0400

    funcionalidade Editar de Pratos

--- SistemaGarsonWeb/Files/Script/Script.js
+++ SistemaGarsonWeb/Files/Script/Script.js
@@ -2,3 +2,11 @@
     var url = $(this).attr("data-url");
     $('.modal-content').load("/"+ url +"/Cadastrar/");
 });
+$(".ModalEditar").click(function () {
+    var url = $(this).attr("data-url");
+    $('.modal-content').load("/"+ url);
+});
+$(".ModalDeletar").click(function () {
+    var url = $(this).attr("data-url");
+    $('.modal-content').load("/" + url);
+});
